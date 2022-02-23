1634101b27ee30789184722407573f437eec0e91
Hannes Bergsten <hannes.bergsten@zingtongroup.com>
2022-02-23 17:02:07 +0100

    POC

---
+++ Extensions/AzureConfigurationHelper.cs
@@ -0,0 +1,32 @@
+﻿using Azure.Identity;
+
+namespace AppConfigurationFetcher.Extensions;
+public static class AzureConfigurationHelper
+{
+    public static void ConnectToAppConfigurationStore(this ConfigurationManager configuration, bool isDevelopment = false, string appConfigurationUrl = "https://my-configuration-store.azconfig.io")
+    {
+        configuration.AddAzureAppConfiguration(options =>
+        {
+            var credentials = new DefaultAzureCredential();
+            if (isDevelopment)
+            {
+                credentials = new DefaultAzureCredential(
+                    new DefaultAzureCredentialOptions
+                    {
+                        VisualStudioTenantId = configuration.GetValue<string>("AADTenantId")
+                    });
+            }
+
+            var labelFilter = "possibleLabelFilter";
+
+            options.Connect(new Uri(appConfigurationUrl), credentials)
+                .Select(keyFilter: "possibleKeyFilter", labelFilter: labelFilter);
+
+            options.ConfigureRefresh(refresh =>
+            {
+                refresh.Register($"RefreshAll", labelFilter, true);
+                refresh.SetCacheExpiration(TimeSpan.FromSeconds(5));
+            });
+        });
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,29 @@
+using AppConfigurationFetcher.Extensions;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var services = builder.Services;
+var configuration = builder.Configuration;
+
+configuration.ConnectToAppConfigurationStore(builder.Environment.IsDevelopment());
+
+services.Configure<Settings>(builder.Configuration.GetSection("MyServiceSection"));
+services.AddScoped(cfg => cfg.GetService<IOptionsSnapshot<Settings>>().Value);
 
 var app = builder.Build();
 
-app.MapGet("/weatherforecast", () =>
+app.UseAzureAppConfiguration();
+
+app.MapGet("/", (Settings settings) =>
 {
-    return "s";
+    return $"AppConfigurationStoreSecret: {settings.AppConfigurationStoreSecret}. AppSettingsProp: {settings.AppSettingsProp}";
 }); 
 
 app.Run();
+
+public class Settings
+{
+    public string AppSettingsProp { get; set; }
+    public string AppConfigurationStoreSecret { get; set; }
+}
